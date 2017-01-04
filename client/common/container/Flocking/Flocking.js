f9b4c1eb2b2ec237d292128a53ebea10d9c954a7
henryyp <henryyp@monkiki.co>
2017-01-04 17:53:42 +0000

    update'

--- client/common/container/Flocking/Flocking.js
+++ client/common/container/Flocking/Flocking.js
@@ -1,28 +1,35 @@
-import AbstractThreeIndex from 'container/Basic/AbstractThreeIndex'
+import BasicThreeWithCam from 'container/Basic/BasicThreeWithCam'
+import GPUComputationRenderer from 'lib/GPUComputationRenderer'
 import React from 'react'
-import * as THREE from  'three'
+import * as THREE from 'three'
 
-export default class Flocking extends AbstractThreeIndex {
+export default class Flocking extends BasicThreeWithCam {
 
   constructor() {
     super()
+  }
+
+  init() {
+    super.init()
+
+    let { width, height } = this.state
+    this.gpuCompute = new GPUComputationRenderer( width, height, this.renderer )
+
   }
 
 
 
   render() {
     return(
-      <div
-        ref = { c => { this.container = c }}
-        className="day__container"
+      <div ref = { c => { this.container = c }}
+        className="section__container"
         onMouseUp={this.mouseUp}
         onMouseDown={this.mouseDown}
-        onMouseMove={this.mouseMove}
-        onMouseWheel={this.mouseWheel}
-        onTouchStart={this.touchStart}
-        onTouchMove={this.touchMove}
-        onTOuchEnd={this.touchEnd}>
+        onWheel={this.mouseWheel}
+        onMouseOut={this.mouseUp}
+        onMouseMove={this.mouseMove}>
       </div>
     )
   }
+
 }
